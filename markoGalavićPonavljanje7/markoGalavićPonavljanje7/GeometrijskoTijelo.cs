78bfb2c2e5b8a32718b16bdeeb0cf7aa2475220c
markogalavic <55134792+markogalavic@users.noreply.github.com>
2020-12-04 12:59:19 +0100

    Add files via upload

---
+++ markoGalavićPonavljanje7/markoGalavićPonavljanje7/GeometrijskoTijelo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace markoGalavićPonavljanje7
+{
+    class GeometrijskoTijelo
+    {
+        double volumen = 0;
+        double oplosje = 0;
+        static int brojacTijela=0;
+
+        class Kugla
+        {
+
+        }
+        class Kvadar
+        {
+
+        }
+        Console.ReadKey();
+    }
+}
